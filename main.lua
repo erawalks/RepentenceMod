1f40473976d20658dc4a5c36c82044f2ac940218
MrSeemsGood <87701031+MrSeemsGood@users.noreply.github.com>
2021-07-29 23:13:34 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,1 +1,7 @@
 local game = Game()
+local RepPlus = RegisterMod("Repentance Plus", 1)
+
+local Collectibles = {
+  ORDLIFE = Isaac.GetItemIdByName("Ordinary Life")
+  MISSINGMEMORY = Isaac.GetItemIdByName("Missing Memory")
+}
