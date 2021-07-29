ec2b29ffeb6a3421f8b7c7735d6ead3c1a697862
MrSeemsGood <87701031+MrSeemsGood@users.noreply.github.com>
2021-07-30 00:19:07 +0300

    !

--- main.lua
+++ main.lua
@@ -1,7 +1,12 @@
 local game = Game()
 local RepPlus = RegisterMod("Repentance Plus", 1)
+local sfx = SFXManager()
 
 local Collectibles = {
-  ORDLIFE = Isaac.GetItemIdByName("Ordinary Life")
+  ORDLIFE = Isaac.GetItemIdByName("Ordinary Life"),
   MISSINGMEMORY = Isaac.GetItemIdByName("Missing Memory")
+}
+
+local Trinkets = {
+  BASEMENTKEY = Isaac.GetTrinketIdByName("Basement Key")
 }
